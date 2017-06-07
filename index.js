f6265c632842ccc9b486856cceec4dad27af5105
Tom <tomtaoran@aliyun.com>
2017-06-07 02:13:21 -0400

    Finished ALL requirements with poor User Experience

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const inputForm = document.querySelector('#inputForm')
 const todoList = {}
+const promoteList={}
 let todoListCounter = 0;
 const storedList= document.querySelector('#storedList')
+
+
 function renderColor(color){
     const div=document.createElement('div');
     div.style.backgroundColor=color;
@@ -13,8 +16,35 @@ function renderColor(color){
 function renderListItem(label, value){
     const item = document.createElement('li')
     item.innerHTML = `${value}`
+    const promoteButton = document.createElement("button");
+    promoteButton.setAttribute("class",label)
+    promoteButton.innerHTML = "promote";
+    promoteButton.addEventListener('click',handlePromote)
+    //This is redundant, but it seems to be the only way to render on-the-spot
+    promoteButton.addEventListener('click',function(){
+         item.style.color="red"
+    })
+    item.appendChild(promoteButton)
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute("class",label)
+    deleteButton.innerHTML = "delete";
+    deleteButton.addEventListener('click',function(){
+         item.remove()
+         delete todoList[label]
+    })
+    item.appendChild(deleteButton)
+    if(promoteList[label]===1){
+        item.style.color="red"
+    }
     return item
 }
+
+function handlePromote(ev){
+    ev.preventDefault()
+    const b= ev.target
+    promoteList[b.className]=1
+}
+
 function renderList(data){
     const list = document.createElement('ol')
 
@@ -22,6 +52,7 @@ function renderList(data){
     Object.keys(data).reverse().map(function(label){
         const item =renderListItem(label,data[label])
         list.appendChild(item)
+        
     })
     //['name','favoriteColor', 'Age']
     return list
@@ -33,6 +64,7 @@ function handleSubmit(ev){
     todoListCounter= todoListCounter+1
     const content = f.content.value
     todoList[title]=content 
+    promoteList[title]=0
     //Rerender by clear the storedList div first
     while(storedList.firstChild){
     storedList.removeChild(storedList.firstChild);
@@ -56,5 +88,6 @@ function changeAppearance(ev){
   
 }
 inputForm.addEventListener('submit', handleSubmit) 
+
 //personForm.addEventListener('submit', changeAppearance)
 
